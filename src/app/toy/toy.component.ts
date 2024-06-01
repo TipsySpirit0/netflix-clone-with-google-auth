6920e6c47bb665533f206eccb546fc0072a3efa0
Tobiloba <tobitobe66@gmail.com>
2024-06-01 08:57:05 +0100

    functionality and styling improvements

---
+++ src/app/toy/toy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toy',
+  standalone: true,
+  imports: [],
+  templateUrl: './toy.component.html',
+  styleUrl: './toy.component.scss'
+})
+export class ToyComponent {
+
+}
